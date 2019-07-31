4c77e853610f905ea1b0e5a6cdb8c825f137ffa2
GnanaKrishna <35880194+GnanaKrishna@users.noreply.github.com>
2019-07-30 23:26:16 -0500

    Created SQL files to create and drop the tables

---
+++ DROP ALL CREATED TABLES.sql
@@ -0,0 +1,3 @@
+USE krishnadatabase;
+DROP TABLE IF EXISTS address, address_type, agent, bank_account, fedral_tax_transanction, organization, registration,
+			state_tax_transanction, tax_payer, tax_payer_depedent, user, user_type;
